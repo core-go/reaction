b0a0c53620e3623e2ce04060f4971c43028551fd
thanh phú <htp1912@gmail.com>
2023-04-19 17:05:48 +0700

    Fix bug follow

--- follow/follow_service.go
+++ follow/follow_service.go
@@ -93,7 +93,7 @@ func (s *followService) Follow(ctx context.Context, id string, target string) (i
 
 	query3 := fmt.Sprintf(
 		"insert into %s(%s, %s, %s) values ($1, 0, 1) on conflict (%s) do update set %s = %s.%s + 1;",
-		s.UserInfoTable, s.UserInfoIdCol, s.FollowerCountCol, s.FollowingCountCol, s.UserInfoIdCol, s.FollowingCountCol, s.FollowingCountCol, s.UserInfoTable)
+		s.UserInfoTable, s.UserInfoIdCol, s.FollowerCountCol, s.FollowingCountCol, s.UserInfoIdCol, s.FollowingCountCol, s.UserInfoTable, s.FollowingCountCol)
 	fmt.Println(query3)
 	stmt3, er0 := s.DB.Prepare(query3)
 	if er0 != nil {
@@ -103,7 +103,7 @@ func (s *followService) Follow(ctx context.Context, id string, target string) (i
 
 	query4 := fmt.Sprintf(
 		"insert into %s(%s, %s, %s) values ($1, 1, 0) on conflict (%s) do update set %s = %s.%s + 1;",
-		s.UserInfoTable, s.UserInfoIdCol, s.FollowerCountCol, s.FollowingCountCol, s.UserInfoIdCol, s.FollowerCountCol, s.FollowerCountCol, s.UserInfoTable)
+		s.UserInfoTable, s.UserInfoIdCol, s.FollowerCountCol, s.FollowingCountCol, s.UserInfoIdCol, s.FollowerCountCol, s.UserInfoTable, s.FollowerCountCol)
 	fmt.Println(query4)
 	stmt4, er0 := s.DB.Prepare(query4)
 	if er0 != nil {
